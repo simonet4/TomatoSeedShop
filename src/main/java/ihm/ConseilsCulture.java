1d2f678a686dee10a721ece0ddb515f81695231a
victor Simonet <102629039+simonet4@users.noreply.github.com>
2025-06-03 16:06:02 +0200

    conseils culture

--- src/main/java/ihm/ConseilsCulture.java
+++ src/main/java/ihm/ConseilsCulture.java
@@ -1,66 +1,42 @@
 package ihm;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+public class ConseilsCulture {
 
-public class ConseilsCulture extends JDialog {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                JFrame frame = new JFrame("Conseils de Culture");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(600, 500);
+                frame.setLocationRelativeTo(null);
+                
+                JPanel panel = new JPanel();
+                frame.getContentPane().add(panel, BorderLayout.NORTH);
+                panel.setLayout(new BorderLayout(0, 0));
+                
+                JScrollPane scrollPane = new JScrollPane((Component) null);
+                scrollPane.setToolTipText(		"Conseils de culture\n\n" +
+											    "Semis : mars-avril\n" +
+											    "Repiquage : après les gelées\n" +
+											    "Récolte : juillet à septembre, voire octobre\n\n" +
+											    "Semis :\n" +
+											    "Démarrez vos semis en petite terrine dès mars / avril (15/20° nuit et jour), dans du terreau à semis, sous une épaisseur de 0,5 cm, tassez légèrement et humidifiez.\n" +
+											    "Repiquer vos semis lorsqu'ils font 5 cm, dans des godets avec du terreau pour semis ou fumier ou compost, enterrer jusqu'à première feuille.\n\n" +
+											    "Après les Saints de glaces, plantez vos pieds de tomates dès qu'ils atteignent 15 cm, les espacer d'au moins 50 cm.\n" +
+											    "Les planter profondément dans un trou avec du fumier ou compost au fond.\n" +
+											    "Tuteurez-les dès la plantation avec des tuteurs d'une hauteur espacés de 70 cm.\n\n" +
+											    "Arrosez abondamment les premiers jours puis arrosez les 15 jours suivants sans mouiller le feuillage.\n\n" +
+											    "Pensez à pailler ! ainsi vous garderez beaucoup plus facilement..."
+											);
+                frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
-	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			ConseilsCulture dialog = new ConseilsCulture();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Create the dialog.
-	 */
-	public ConseilsCulture() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		
-		JTextArea txtrPourRussirLa = new JTextArea();
-		txtrPourRussirLa.setEditable(false);
-		txtrPourRussirLa.setText("Variétés adaptés: Tomate 'Cœur de Bœuf' ou la 'Tomate Cerise'\r\nPériode idéale: Mi-mai et début juin\r\nSol: Riche et drainé avec 3 à 5 kg de compost par mètre carré\r\nExposition au soleil: 6/8h par jour\r\npH du sol: 6 à 6.8\r\nLe plant: Utiliser des tuteurs\r\nArrosage: 5L à 10L par semaine");
-		txtrPourRussirLa.setBounds(10, 10, 416, 212);
-		contentPanel.add(txtrPourRussirLa);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						 dispose();
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-		}
-	}
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
