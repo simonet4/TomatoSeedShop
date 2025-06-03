e88f3faa58c6f25eaf603f3f697c3a442c7d623f
victor Simonet <102629039+simonet4@users.noreply.github.com>
2025-06-03 17:22:39 +0200

    boutons panier

--- src/main/java/ihm/PagePanier.java
+++ src/main/java/ihm/PagePanier.java
@@ -141,6 +141,11 @@ public class PagePanier extends JDialog {
 		panel_4.add(ButtonViderPanier);
 
 		JButton ButtonContinuerAchats = new JButton("Continuer les achats");
+		ButtonContinuerAchats.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		panel_4.add(ButtonContinuerAchats);
 
 		JButton ButtonValiderPanier = new JButton("Valider le panier");
@@ -163,6 +168,7 @@ public class PagePanier extends JDialog {
 						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 				if (response == JOptionPane.YES_OPTION) {
 					clearCart();
+					dispose();
 				}
 			}
 		});
